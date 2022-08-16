8b81aa1b10b7c6221e7eeb7ba5ff380f09b7f2c4
Giovanni <g.salmeri@mondodomani.org>
2022-08-16 09:41:37 +0200

    Fixed indentation

--- backtotop.js
+++ backtotop.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
         link.style.opacity = "1";
         link.style.visibility = "visible"; // accessibility
     } else {
-    window.addEventListener("scroll", function() {
-        if ((document.body.scrollTop || document.documentElement.scrollTop) > screens*window.innerHeight) {
-            link.style.opacity = "1";
-            link.style.visibility = "visible"; // accessibility
-        } else {
-            link.style.opacity = "0";
-            link.style.visibility = "hidden"; // accessibility
-        }
-    });
+        window.addEventListener("scroll", function() {
+            if ((document.body.scrollTop || document.documentElement.scrollTop) > screens*window.innerHeight) {
+                link.style.opacity = "1";
+                link.style.visibility = "visible"; // accessibility
+            } else {
+                link.style.opacity = "0";
+               link.style.visibility = "hidden"; // accessibility
+            }
+        });
     }
 });
